a412dd8c6dac47230ef3ec282e491621b70457ee
Shameer412 <muhammadshameer522@gmail.com>
2024-08-23 22:40:30 +0500

    first commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Muhammad Shameer</span>  <br><br>  I am an experienced frontend developer specializing in React and JS. I have a passion for building high-performance, responsive websites and web applications using the latest technologies.I am a creative Front-end Developer (HTML/CSS/Js/Tailwind CSS/React.Js) with many years of experience in developing professional responsive websites, Landing pages, and web applications.I’m an EXPERT in converting design files (Figma, XD, Sketch, and PSD) into pixel-perfect, semantic (HTML/CSS/JavaScript/Bootstrap/Tailwind CSS/React.Jss ) with Fully responsive, SEO Friendly, Clean, and W3C Validated coding."
+      ],
+      typeSpeed: 12,
+      contentType: 'html'
+    });
+  }, 100);
